446fe8cc004bc522b593df886858c4721ed64c1c
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-08-02 23:56:18 -0700

    Bigram matrix

---
+++ README.md
@@ -0,0 +1,18 @@
+# KiwiGPT
+
+## Install
+
+```sh
+uv pip install -e .
+```
+
+```sh
+uv run jupyter notebook \
+  --no-browser \
+  --port=8889 \
+  --NotebookApp.ip='127.0.0.1'
+```
+
+```sh
+uv run jupyter notebook
+```
---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    git # Git
+    python311 # Python 3.11
+    uv # Python package manager
+    just # Just
+    nixfmt-classic # Nix formatter
+    alejandra # Nix formatter
+    quarto # Quarto
+  ];
+
+  # Shell hook to set up environment
+  shellHook = ''
+  '';
+}
+
